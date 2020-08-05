760b5ef2ed2d60bbaad6fd25d3a25d518509d367
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-05 13:54:44 +0700

    update-Web

---
+++ ContactBookWeb/Models/Student/GetStudentByClassId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookWeb.Models.Student
+{
+    public class GetStudentByClassId
+    {
+        public int ClassId { get; set; }
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string DayOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+    }
+}
--- ContactBookWeb/Startup.cs
+++ ContactBookWeb/Startup.cs
@@ -50,8 +50,8 @@ namespace ContactBookWeb
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-             
+                    pattern: "{controller=Home}/{action=Index}/{id?}");          
+
             });
         }
     }
