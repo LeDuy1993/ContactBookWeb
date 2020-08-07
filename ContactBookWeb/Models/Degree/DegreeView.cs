214a00cd90cedf7cae7ff5afe36d3892cb7a2ebe
an hong <hongan529@gmail.com>
2020-08-07 11:49:50 +0700

    add teacher

---
+++ ContactBookWeb/Models/Degree/DegreeView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookWeb.Models.Degree
+{
+    public class DegreeView
+    {
+        public int DegreeId { get; set; }
+        public string DegreeName { get; set; }
+    }
+}
