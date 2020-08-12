2e43f2e630aa5dfe4063ac5616c219a88ff84873
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-12 10:38:36 +0700

    Duy_TablePoint_Fix (Model_Controller_js)

--- ContactBookWeb/wwwroot/js/contacBook.js
+++ ContactBookWeb/wwwroot/js/contacBook.js
@@ -40,7 +40,7 @@ contactBook.showStudent = function () {
                     $('#studentId').append(
                         `
                     <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab"  role="tab" aria-selected="true"  href="javascript:;" onclick="contactBook.showPoint({${v.studentId})" >${v.firstName} ${v.lastName} </a>
+                        <a class="nav-link active" data-toggle="tab"  role="tab" aria-selected="true"  href="javascript:;" onclick="contactBook.showPoint(${v.studentId})" >${v.firstName} ${v.lastName} </a>
                     </li>
                      `
                     );
@@ -49,13 +49,74 @@ contactBook.showStudent = function () {
                     $('#studentId').append(
                         `
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab"  role="tab" aria-selected="false"href="javascript:;" onclick="contactBook.showPoint({${v.studentId})">${v.firstName} ${v.lastName} </a>
+                        <a class="nav-link" data-toggle="tab"  role="tab" aria-selected="false" href="javascript:;" onclick="contactBook.showPoint(${v.studentId})">${v.firstName} ${v.lastName} </a>
                     </li>
                      `
                     );
                 }
                 count += 1;
 
+            });
+        }
+    });
+}
+contactBook.showPoint = function (studentId) {
+    courseId = $('#courseId').val();
+    classId = $('#classId').val();
+    $.ajax({
+        url: `/ContactBook/ShowTablePoint/${courseId}/${studentId}/${classId}`,
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#point1st').empty();
+            $('#point2st').empty();
+            $.each(data.tableContactBook.subjectPoint1st, function (i, v) {
+                $('#point1st').append(
+                `
+                         <tr>
+                          <td>${v.subjectId}</td>
+                          <td>${v.subjectName}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point1st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point2st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point3st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point4st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point5st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point6st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point7st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point8st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point9st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point10st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point11st}</td>
+                          <td style="padding: 0px; width: 5%;" type="number" min="0" max="10" required="true">${v.avg}
+                            </td >
+                  
+                      </tr>
+                 `
+                )
+            });
+            $.each(data.tableContactBook.subjectPoint2st, function (i, v) {
+                $('#point2st').append(
+                    `
+                         <tr>
+                          <td>${v.subjectId}</td>
+                          <td>${v.subjectName}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point1st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point2st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point3st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point4st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point5st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point6st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point7st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point8st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point9st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point10st}</td>
+                          <td style="padding: 0px; width: 5%;" >${v.point11st}</td>
+                          <td style="padding: 0px; width: 5%;" type="number" min="0" max="10" required="true">${v.avg}
+                            </td >
+                  
+                      </tr>
+                 `
+                )
             });
         }
     });
