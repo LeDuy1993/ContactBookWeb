18795799f15465a1f89247b5a115fd8818e81ec3
an hong <hongan529@gmail.com>
2020-08-14 10:36:58 +0700

    An_FrontIndex(Controller-Layout-css)

---
+++ ContactBookWeb/Controllers/ThptADDController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookWeb.Controllers
+{
+    public class ThptADDController :Controller
+    {
+        private readonly ILogger<ThptADDController> _logger;
+
+        public ThptADDController(ILogger<ThptADDController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
