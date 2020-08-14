b6e316c3dfb6f6194a495e513404c57bcd470d2f
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-14 23:26:50 +0700

    Duy_SubjectResult_Models_Update

--- ContactBookWeb/Models/SubjectResult/GetSubjectResultBySubjectResultId.cs
+++ ContactBookWeb/Models/SubjectResult/GetSubjectResultBySubjectResultId.cs
@@ -8,10 +8,9 @@ namespace ContactBookWeb.Models.SubjectResult
     public class GetSubjectResultBySubjectResultId
     {
         public int SubjectResultId { get; set; }
-        public int ClassId { get; set; }
         public int SemesterId { get; set; }
-        public int SubjectId { get; set; }
-        public int StudentId { get; set; }
+        public int ClassSubjectId { get; set; }
+        public int ClassStudentId { get; set; }
         public string ListPoint { get; set; }
         public string ListDate { get; set; }
     }
