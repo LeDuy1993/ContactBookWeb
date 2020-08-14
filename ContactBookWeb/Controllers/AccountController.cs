8cc44577fd8203219ac607fe89bf07eceb5e9e46
tranhuuduc215 <tranhuuduc215@gmai.com>
2020-08-14 10:41:33 +0700

    Duc: create login and add image folder

--- ContactBookWeb/Controllers/AccountController.cs
+++ ContactBookWeb/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactBookWeb.Models.Account;
+using ContactBookWeb.Ultilities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,26 +11,71 @@ namespace ContactBookWeb.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
-
-        public AccountController(ILogger<AccountController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult ForgotPass()
-        { 
 
-            return View();
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = new LoginResult();
+                var loginRequest = new LoginRequest()
+                {
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                result = ApiHelper<LoginResult>.HttpPostAsync(
+                                                        $"{Helper.ApiUrl}api/account/login",
+                                                        loginRequest
+                                                    );
+                if (result.Success)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
+            return View(model);
         }
+
         public IActionResult Register()
         {
-
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var registerRequest = new RegisterRequest()
+                {
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                var result = new RegisterResult();
+                result = result = ApiHelper<RegisterResult>.HttpPostAsync(
+                                                        $"{Helper.ApiUrl}api/account/register",
+                                                        registerRequest
+                                                    );
+                if (result.Success)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            var result = true;
+            return Json(new { result });
         }
     }
 }
