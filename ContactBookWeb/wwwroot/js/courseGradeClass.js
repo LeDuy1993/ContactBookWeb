ae5ecf91e9707c23c2d5ee03480822e3d75ac136
Leduyjapan1993@gmail.com <Leduyjapan1993@gmail.com>
2020-08-16 11:45:46 +0700

    Duy_courseGradeClass.js_Add

---
+++ ContactBookWeb/wwwroot/js/courseGradeClass.js
@@ -0,0 +1,15 @@
+﻿var courseGradeClass = {} || courseGradeClass;
+var courseId = 0;
+
+
+
+
+
+courseGradeClass.init = function () {
+    courseGradeClass.showClass();
+};
+
+$(document).ready(function () {
+    courseGradeClass.init();
+    courseId = $('#courseId').val();
+});
