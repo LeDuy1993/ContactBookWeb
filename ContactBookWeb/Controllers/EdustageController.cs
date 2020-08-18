5b2e0004d391be822cfd57e2fba51b7c5b3be98f
an hong <hongan529@gmail.com>
2020-08-18 08:31:30 +0700

    An_Fix Front end Trang chu (Controller-Views-wwwroot)

---
+++ ContactBookWeb/Controllers/EdustageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookWeb.Controllers
+{
+    public class EdustageController :Controller
+    {
+        private readonly ILogger<EdustageController> _logger;
+
+        public EdustageController(ILogger<EdustageController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
